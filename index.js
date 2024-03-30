09f431987324ab66d59a9a06ca610cc4ec6ca279
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-03-30 23:05:16 +0300

    functions Cont'd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// creating a fuction called saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+// create a function called mondaywork
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+// Creation of a function known as wrapAdjective
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
